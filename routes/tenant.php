f77f687fd837354e7447002ccd1bcfbaee3eabec
travelboostid <travelboost.id@gmail.com>
2026-04-02 10:05:16 +0700

    fix: routing errors

--- routes/tenant.php
+++ routes/tenant.php
@@ -2,15 +2,11 @@
 
 use App\Http\Controllers\Tenant\HomeController;
 use App\Http\Controllers\Tenant\TourController;
+use App\Models\Tour;
 use Illuminate\Support\Facades\Route;
 
 $appHost = env('APP_HOST', 'localhost');
 Route::domain('{username}.' . $appHost)->group(function () {
-  // log current host
-
-  //31032026
-  Route::get('/', [TourController::class, 'index']);
-  //Route::get('/', [HomeController::class, 'index']);
-  Route::get('/tours', [HomeController::class, 'index']);
-  //Route::get('/tours', [TourController::class, 'index']);
+  Route::get('/', [HomeController::class, 'index']);
+  Route::get('/tours', [TourController::class, 'index']);
 });
