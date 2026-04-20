90479425ae14d83d68ed01105412e231a16418d4
erasoft-tekno <54024880+erasoft-tekno@users.noreply.github.com>
2026-04-20 11:05:32 +0700

    commit boostrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use App\Http\Middleware\DomainResolver;
 
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
