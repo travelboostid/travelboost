bf10443764acdd465f4e5ee6c55b9cc682c5df35
travelboostid <travelboost.id@gmail.com>
2026-05-08 08:04:18 +0700

    fix: show meny by role

--- resources/js/components/layouts/std-layout/nav-user.tsx
+++ resources/js/components/layouts/std-layout/nav-user.tsx
@@ -2,26 +2,30 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { DEFAULT_PHOTO } from '@/config';
 import usePageSharedDataProps from '@/hooks/use-page-shared-data-props';
+import { dashboard as adminDashboard } from '@/routes/admin';
+import { index as companyDashboard } from '@/routes/companies/dashboard';
+import { index as userDashboard } from '@/routes/me';
 import { Link } from '@inertiajs/react';
 import { UserIcon } from 'lucide-react';
 import { useMemo } from 'react';
-import { useAvailableDashboards } from '../components/hooks';
 
 export function NavUser() {
   const { auth } = usePageSharedDataProps();
-  const dashboards = useAvailableDashboards();
-  const defaultDashboard = useMemo(() => {
-    if (auth.roles.includes('user:admin')) {
-      return dashboards.find((d) => d.id === 'admin:default') || dashboards[0];
-    } else {
-      return (
-        dashboards.find((d) => d.id.startsWith('company:')) || dashboards[0]
-      );
-    }
-  }, [auth.roles, dashboards]);
+  const defaultDashboardUrl = useMemo(() => {
+    const company = auth.user.companies[0];
+    const dashboards = {
+      'user:customer': userDashboard().url,
+      'user:vendor': companyDashboard({ username: company.username }).url,
+      'user:agent': companyDashboard({ username: company.username }).url,
+      'user:admin': adminDashboard().url,
+    } as Record<string, string>;
+    const userRole =
+      auth.roles.find((r) => r.startsWith('user:')) || 'user:customer';
+    return dashboards[userRole] || userDashboard().url;
+  }, [auth.roles, auth.user.companies]);
   return (
     <Button asChild variant="ghost" className="gap-2 px-2">
-      <Link href={defaultDashboard.baseUrl}>
+      <Link href={defaultDashboardUrl}>
         <Avatar className="size-6 rounded-lg">
           <AvatarImage
             src={auth.user.photo_url || DEFAULT_PHOTO}
